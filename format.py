947610d4fd89afb374d79073d4f29f8a701e462b
ayoubbargach <bargachayoub@gmail.com>
2018-12-04 00:29:30 +0100

    New format function

--- format.py
+++ format.py
@@ -1,8 +1,42 @@
-from math import *
-import numpy as np
-import ast
 
+# Add a function to format white spaces
+
+	
+# Recursive function to format
+def recurs_array(text, i = 0):
+	if text[i] == '[':
 		
+		result = []
+		counter = 0
+		i += 1
+		print (i)
+		while (True) :
+			tmp = recurs_array(text, i)
+			print(tmp)
+			if  tmp[0] == None :
+				if counter == 1 :
+					print("HERE")
+					return result[0], tmp[1]
+				else :
+					return result, tmp[1]
+			else :
+				counter += 1
+				result.append(tmp[0])
+				i = tmp[1]
+
+	elif text[i] == ']' :
+		return None, i+1
+	elif text[i].isdigit():
+		# We start by locating the next ]
+		end = text.index(']', i)
+
+		# Then we split by using the indexes and the parameter whitespace
+		result = text[i:end].split(" ")
+		return result, end
+	else :
+		return None, i
+	
+
 	
 filename = "CNN_coeff_3x3.txt"
 
@@ -31,11 +65,17 @@ def get_array(filename):
 		# let us loop again in each key and generate an array for each of them
 		# Note that we simply replace the raw data with the generated array
 		for key, value in content.iteritems() :
-    			print("Generating an array for the key " + key)
-			value = ast.literal_eval(value)
+			print("Generating an array for the key " + key)
+			# value = ast.literal_eval(value)
 			print value
 			
 		
 
+test_list = "[[1 0][[1 0][2 3]]]"
 
-get_array(filename)
+print("GO")
+
+r =recurs_array(test_list)
+
+print ("GO")
+print(r)
