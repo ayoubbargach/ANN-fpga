c8142649f0711f787274fbc75ef4c4a345d42670
ayoubbargach <bargachayoub@gmail.com>
2018-12-04 23:10:58 +0100

    New version format.py (finished)

--- format.py
+++ format.py
@@ -1,13 +1,11 @@
 
 # Add a function to format white spaces
 def clean(text):
-	text = text.replace("  ", " ")
+	text = ' '.join(text.split()) # remove all multiple spaces
 	text = text.replace(" [", "[")
 	text = text.replace(" ]", "]")
 	text = text.replace("[ ", "[")
 	text = text.replace("] ", "]")
-
-	print(text)
 
 	return text
 	
@@ -15,38 +13,49 @@ def clean(text):
 
 	
 # Recursive function to format
-def recurs_array(text, i = 0):
-	if text[i] == '[':
-		
-		result = []
-		counter = 0
-		i += 1
-		print (i)
-		while (True) :
-			tmp = recurs_array(text, i)
-			print(tmp)
-			if  tmp[0] == None :
-				if counter == 1 :
-					print("HERE")
-					return result[0], tmp[1]
-				else :
-					return result, tmp[1]
-			else :
-				counter += 1
-				result.append(tmp[0])
-				i = tmp[1]
+def recursive_array(text, i=0):
+	if (len(text) < i):
+		return None, i
+	elif text[i] == '[':
 
-	elif text[i] == ']' :
-		return None, i+1
-	elif text[i].isdigit():
+		#Increment i to avoid char '['
+		i += 1
+
+		# Before doing more complicated stuff, let us analyse the following char to predict the behavior
+
+		if text[i].isdigit() or text[i] == '-':
+			return recursive_array(text, i)
+		
+		else :
+			# We open a new context
+			context = []
+
+			while (True):
+				element = recursive_array(text, i)
+
+				# Lets save now the new i for the potentiel next iteration 
+				i = element[1]
+
+				if (element[0] == None):
+					# Then we have to close the context
+					return context, i
+				else :
+					context.append(element[0])
+
+	elif text[i].isdigit() or text[i] == '-':
 		# We start by locating the next ]
 		end = text.index(']', i)
 
 		# Then we split by using the indexes and the parameter whitespace
 		result = text[i:end].split(" ")
-		return [float(e) for e in result], end
+
+		# We convert each element and return it as a list of floats
+		return [float(e) for e in result], end+1
+	
 	else :
-		return None, i
+		return None, i+1
+
+					
 	
 
 	
@@ -55,7 +64,6 @@ filename = "CNN_coeff_3x3.txt"
 def get_array(filename):
 	""" Returns an array formatted from the filename issued	"""
 
-	output_array = []
 	key = ""
 	content = {} # We create a dictionary to keep track of all informations
 
@@ -78,16 +86,13 @@ def get_array(filename):
 		# Note that we simply replace the raw data with the generated array
 		for key, value in content.iteritems() :
 			print("Generating an array for the key " + key)
-			# value = ast.literal_eval(value)
-			print value
+			
+			value = recursive_array(clean(value))
+			print(value)
+			
+
+
+	return content
 			
 		
-
-test_list = "[[ 1 0][[1 0]  [2 3 ]  ]]"
-
-print("GO")
-
-r =recurs_array(clean(test_list))
-
-print ("GO")
-print(r)
+content = get_array(filename)
